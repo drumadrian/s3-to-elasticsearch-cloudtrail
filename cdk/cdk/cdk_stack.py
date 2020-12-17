7226aa5c0a3f706444d9889c6d75f82591599a28
Adrian Drummond <adrian.drummond@gmail.com>
2020-12-16 19:33:07 -0800

    fixing lambda function permissions

--- cdk/cdk/cdk_stack.py
+++ cdk/cdk/cdk_stack.py
@@ -97,7 +97,7 @@ class CdkStack(core.Stack):
         ###########################################################################
         lambda_supplemental_policy_statement = aws_iam.PolicyStatement(
             effect=aws_iam.Effect.ALLOW,
-            actions=["s3:Get*","s3:Head*","s3:List*","firehose:*"],
+            actions=["s3:Get*","s3:Head*","s3:List*","firehose:*","es:*"],
             resources=["*"]
             )
 
